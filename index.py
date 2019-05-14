7c89bb32c54d39699e1bafdedd49905cf864451b
“Julie-S-Bernal“ <Julies.bernal@gmail.coml>
2019-05-14 23:18:29 +0100

    fix some issue in python game

--- index.py
+++ index.py
@@ -4,15 +4,15 @@ from random import randint
 player_wins = 0
 computer_wins = 0
 player2_wins = 0
-print("Rock, Paper and Scissors")
+print('Rock, Paper and Scissors')
 
-player1 = input("Do you want to play with a friend or computer? Type friend or computer to pick")
+player1 = raw_input('Do you want to play with a friend or computer? Type friend or computer to pick')
 
-if player1== 'friend':
+if player1 == 'friend':
     while player_wins < 5 and player2_wins < 5:
-        player1 = input("Player 1, pick your weapon: ")
-        player2 = input("Player 2, pick your weapon: ")
-         print(f"Player Score: {player_wins} computer score{player2_wins}")
+        player1 = raw_input('Player 1, pick your weapon: ')
+        player2 = raw_input('Player 2, pick your weapon: ')
+        # print(f"player score: {player_wins} computer score: {player2_wins}.") 
         if (player1 == 'rock' and player2 =='scissors') or (player1 == 'scissors' and player2 == 'paper') or (player1 == 'paper' and player2 == 'rock'):
             print('Player 1 wins!')
             player_wins += 1
@@ -24,14 +24,14 @@ if player1== 'friend':
             print("player2 won!")
             player2_wins += 1
     if player_wins > player2_wins:  
-        print(f"FINAL SCORE: Player Score: {player_wins} player2 score{player2_wins}")
+        printf("FINAL SCORE: Player Score: {player_wins} player2 score{player2_wins}")
     else: print("Your friend is better, maybe you should play something else!!!")
 
 elif player1 == 'computer':
     print("Score 5 points to win!!!")
     while player_wins < 5 and computer_wins < 5:
 
-        print(f"Player Score: {player_wins} computer score{computer_wins}")
+        printf("Player Score: {player_wins} computer score{computer_wins}")
         player = input("Player, pick your weapon: ").lower()
         rand_num = randint(0,2)
         if rand_num == 0:
@@ -41,7 +41,7 @@ elif player1 == 'computer':
         else:
             computer = "scissors"
 
-        print(f"Computer plays {computer}" )
+        printf("Computer plays {computer}" )
 
         if player == computer:
             print("It's a tie!")
@@ -67,7 +67,7 @@ elif player1 == 'computer':
     else:
         print("Please enter a valid move!")
     if player_wins > computer_wins:  
-        print(f"FINAL SCORE: Player Score: {player_wins} computer score {computer_wins}")
+        printf("FINAL SCORE: Player Score: {player_wins} computer score {computer_wins}")
     else: print("You are bad!!!!")
 
 
